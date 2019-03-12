3f3f0411be3b733e18d4703cdf28b728c3cc3621
ChrRaz <chr@raznet.dk>
2019-03-12 22:35:28 +0100

    Remove some old stuff

--- src/ursula/core.clj
+++ src/ursula/core.clj
@@ -1,24 +1,6 @@
 (ns ursula.core
   (:gen-class))
 
-(defprotocol GameState
-  (player [s] "Which player has the move in a state")
-  (chance-node? [s] "Is s a chance-node?")
-  (actions [s] "The set of legal moves in the state")
-  (result [s a] "The result of a move")
-  (terminal? [s] "Is the game over?")
-  (utility [s p] "The utility of the state to player p")
-  )
-
-#_(def initial-state
-    {:game/turn :player/white
-     :player/white {:pregame 7
-                    :board #{}
-                    :postgame 0}
-     :player/black {:pregame 7
-                    :board #{}
-                    :postgame 0}})
-
 (def initial-state
   {:game/turn :player/white
    :game/dice nil
