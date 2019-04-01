b0a839d0962e2080156fc08ea13cf69520012ef0
ChrRaz <chr@raznet.dk>
2019-04-01 22:35:36 +0200

    Ship it!

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject ursula "0.1.0-SNAPSHOT"
+(defproject ursula "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [proto-repl "0.3.1"]]
+                 [org.clojure/core.match "0.3.0-alpha5"]]
   :main ^:skip-aot ursula.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ursula/core.clj
+++ src/ursula/core.clj
@@ -35,15 +35,34 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (ui/present-agents agents)
-  (let [player1 (ui/get-choice "Select white player" agents)
-        player2 (ui/get-choice "Select black player" agents)
-        [winning-move final-board] (last
-                                    (game/run-game initial-state
-                                                   {:player/white (:ai/fn player1)
-                                                    :player/black (:ai/fn player2)}
-                                                   ui/print-board))]
-    (println)
-    (if (= 1 (game/utility final-board))
-      (println "White player wins!")
-      (println "Black player wins!"))))
+  (if args
+    (time (let [n (Integer/parseInt (nth args 2))
+                a1 (nth agents (Integer/parseInt (nth args 0)))
+                a2 (nth agents (Integer/parseInt (nth args 1)))]
+            (prn (:ai/name a1) (:ai/name a2) n)
+            (println (.. Runtime getRuntime availableProcessors) "available processors")
+            (->> (repeat n initial-state)
+                 (pmap #(->> (game/run-game %
+                                            {:player/white (:ai/fn a1)
+                                             :player/black (:ai/fn a2)}
+                                            nil)
+                             last
+                             second
+                             game/utility))
+                 (doall)
+                 (reduce + 0)
+                 println)
+            (shutdown-agents)))
+    (do
+      (ui/present-agents agents)
+      (let [player1 (ui/get-choice "Select white player" agents)
+            player2 (ui/get-choice "Select black player" agents)
+            [winning-move final-board] (last
+                                        (game/run-game initial-state
+                                                       {:player/white (:ai/fn player1)
+                                                        :player/black (:ai/fn player2)}
+                                                       ui/print-board))]
+        (println)
+        (if (= 1 (game/utility final-board))
+          (println "White player wins!")
+          (println "Black player wins!"))))))
